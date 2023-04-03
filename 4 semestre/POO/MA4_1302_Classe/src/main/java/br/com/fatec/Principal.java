d5d7e7c2d1fe584fa75dcb9ccd55f92c8d7968be
Leonardo Reis <71906441+leonardoreiss@users.noreply.github.com>
2023-04-03 05:20:26 -0300

    Delete 4 semestre/POO directory

--- 4 semestre/POO/MA4_1302_Classe/src/main/java/br/com/fatec/Principal.java
+++
@@ -1,7 +0,0 @@
-package br.com.fatec;
-public class Principal {
-    //metodo principal do projeto
-    public static void main(String[] args) {
-        System.out.println("Hello World, quem quer ir pra casa? EUUU");
-    }
-}
